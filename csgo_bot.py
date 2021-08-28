075af3250daf316f81d44f44caea58c577ec0cec
Samuel Elmer <selmer@tamu.edu>
2021-08-28 15:00:49 -0500

    Update bot to use commands extension. Fixed api link and added testing command

--- csgo_bot.py
+++ csgo_bot.py
@@ -1,25 +1,34 @@
 import discord
+from discord.ext import commands
 import pip._vendor.requests as requests
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="$")
 
-@client.event
+@bot.event
 async def on_ready():
-    print("Bot logged in as {0.user}".format(client))
+    print("Bot logged in as {0.user}".format(bot))
 
-@client.event
+@bot.command()
+async def test(ctx):
+    await ctx.send(":moyai:")
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
     if message.content.startswith("$hltvresults"):
         try:
             num_games = int(message.content[12:])
-            game_requests = requests.get("https://hltv-api.vercel.app/api/results")
+            game_requests = requests.get("https://hltv-api.vercel.app/api/results.json")
             game_data = game_requests.json()
             for i in game_data[0:num_games]:
                 await message.channel.send("Event: " + str(i["event"]) + "\n" + "Teams: " + str(i["team1"]["name"]) + " - " + str(i["team2"]["name"]) + "\n" + "Score: " + str(i["team1"]["result"]) + " - " + str(i["team2"]["result"]))
         except:
             await message.channel.send("Incorrect command usage. Proper usage: $hltvresults [number]")
 
-client.run("Token")
+    await bot.process_commands(message)
+
+token = ""
+
+with open("token.txt","r") as file:
+    token = file.readline()
+
+bot.run(token)
